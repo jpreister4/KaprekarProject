e17c511561bcd0d4aa126a61058d910200c14b33
jpreister4 <54559118+jpreister4@users.noreply.github.com>
2019-09-13 18:20:50 -0500

    unused variable
    deleted unused variabe "numDigits"

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -32,7 +32,6 @@ int isKaprekar(int n) {
   //compute the square
   long square = n * (long) n;
   //compute the number of digits of the square
-  int numDigits = (int) log10(n) + 1;
   long modulus = 0;
   long first, second;
 
